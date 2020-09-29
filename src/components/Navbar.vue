aa2559f3a58c2d447aa4e31c12ce6d0bd79757a8
sj30001 <59249674+sj30001@users.noreply.github.com>
2020-09-29 14:18:30 +0800

    Delete Navbar.vue

--- src/components/Navbar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nav>
-    <div class="nav-wrapper-green">
-      <div class="container">
-        <router-link to="/" class="brand-logo">Voter Manager</router-link>
-      </div>
-    </div>
-  </nav>
-</template>
