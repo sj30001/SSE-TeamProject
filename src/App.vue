31eb72bcac1c3febca3e3eefeed1a800e81ee079
sj30001 <stefanjia@hotmail.com>
2020-09-29 14:09:53 +0800

    register and signin page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home Page</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/signup">SignUp</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+
+
 
 <style>
 #app {
---
+++ src/views/SignUp.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="signup">
+    <div class="background">
+      <img :src="imgSrc" width="10%" height="10%" alt="" />
+    </div>
+    <br />
+    <br />
+    <h1>Australian Senate Voting System </h1>
+    <br />
+    <h1>Please Sign Up</h1>
+    <br />
+    <input type="email" placeholder="Email" v-model="email">
+    <br />
+    <br />
+    <input type="password" placeholder="Password" v-model="password">
+    <br />
+    <br />
+    <button @click="signUp">Sign Up</button>
+    <br />
+    <br />
+    <p>Have an account?
+      <router-link to="/login">Login</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+export default {
+  name: "SignUp",
+  data(){
+    return{
+      email:"",
+      password:"",
+      imgSrc:require('D:\\vueProj\\votesystem\\src\\views\\img\\11.png')
+    }
+  },
+  //methods handles login and register
+  methods:{
+    signUp: function(){
+      //log the user in
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+          user =>{
+            console.log(user.data)
+          },
+          err => {
+            alert(err);
+          }
+      )
+    }
+
+  },
+}
+</script>
+
+<style>
+</style>
