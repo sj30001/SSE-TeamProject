1dfdc3fc09397ba9b4d3d6171caa4ee577db1fd9
sj30001 <59249674+sj30001@users.noreply.github.com>
2020-09-29 14:18:48 +0800

    Delete firebaseConfig.js

--- src/components/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyD-EcVK9Lp9NHwqrAtuOww6IZ9JzZjcojc",
-  authDomain: "vuevott2.firebaseapp.com",
-  databaseURL: "https://vuevott2.firebaseio.com",
-  projectId: "vuevott2",
-  storageBucket: "vuevott2.appspot.com",
-  messagingSenderId: "298529242306",
-  appId: "1:298529242306:web:ed46d7cb106a5f6aafcf5b"
-}
