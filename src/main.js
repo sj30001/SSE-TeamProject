0f750e774a4d24bdaec8b45d4c5e7555d0047953
sj30001 <stefanjia@hotmail.com>
2020-10-01 15:36:29 +0800

    Add basic framework

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import firebase from "firebase";
 import './styles/style.css'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 
 Vue.config.productionTip = false
 
