0c8d35862e37063c3a2296eb63c395ddc7870768
humaoxuan <19950410@qq.com>
2020-10-03 15:40:02 +0930

    fix absolute path bug

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home Page</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/signup">SignUp</router-link>
-    </div>
-    <router-view/>
+    {{  }}
   </div>
 </template>
 
