82e1e922acea0cc19bdb961e1674ee8e998ad8fe
sj30001 <stefanjia@hotmail.com>
2020-10-04 10:55:09 +0800

    roll back

--- src/main.js
+++ src/main.js
@@ -1,23 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import firebase from "firebase";
-import './styles/style.css'
+import axios from '@/api/http'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios,axios)
 
 
 Vue.config.productionTip = false
 
-var firebaseConfig = {
-  apiKey: "AIzaSyA-C-pq6O7O_NYM_bzPWeRP6xRUeY69Ds4",
-  authDomain: "votesystem-9957c.firebaseapp.com",
-  databaseURL: "https://votesystem-9957c.firebaseio.com",
-  projectId: "votesystem-9957c",
-  storageBucket: "votesystem-9957c.appspot.com",
-  messagingSenderId: "115744970842",
-  appId: "1:115744970842:web:e607b826510d9053b0a063"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
 
 new Vue({
   router,
