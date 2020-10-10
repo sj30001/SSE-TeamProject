6cf8f3eddd880cb6e5d7a8d6a3ef75ae4bc41928
deepsky <461843973@qq.com>
2020-10-10 20:38:28 +0800

    update "add candidates" function

--- src/components/console/ConsoleParty.vue
+++ src/components/console/ConsoleParty.vue
@@ -6,7 +6,7 @@
           size="mini"
           @click="handleAdd()">Add</el-button>
       <el-table
-          :data="tableData" style="width: 60%">
+          :data="partyList" style="width: 60%">
 <!--        <el-table-column-->
 <!--            label="Candidates party"-->
 <!--            width="180"-->
@@ -19,19 +19,12 @@
         <el-table-column
             label="Party Name"
             width="180"
-            prop="name">
+            prop="partyList.partyName">
           <template slot-scope="scope">
-            <span><a href="javascript:;"></a>{{ scope.row.name }}</span>
+            <span><a href="javascript:;"></a>{{ scope.row.partyName }}</span>
           </template>
-        </el-table-column><el-table-column
-          label="States"
-          width="180"
-          prop="States">
-        <template slot-scope="scope">
+        </el-table-column>
 
-          <span>{{ scope.row.state }}</span>
-        </template>
-      </el-table-column>
         <el-table-column label="Operation">
           <template slot-scope="scope">
             <el-button
@@ -46,18 +39,13 @@
       </el-table>
       <el-dialog title="add" :visible.sync="addDialogFormVisible">
         <el-form :model="form">
-          <el-form-item label="State">
-            <el-input v-model="AddObj.state" auto-complete="off"></el-input>
+          <el-form-item label="Party Name">
+            <el-input v-model="AddObj.partyName" auto-complete="off"></el-input>
           </el-form-item>
 <!--          <el-form-item label="Name">-->
 <!--            <el-input v-model="AddObj.candidateName" auto-complete="off"></el-input>-->
 <!--          </el-form-item>-->
-          <el-select v-model="AddObj.party" placeholder="party">
-            <el-option v-for="item in partyList "
-                       :key="item.id"
-                       :label="item.partyName"
-                       :value="item.partyName"></el-option>
-          </el-select>
+
         </el-form>
         <div sloy="footer" class="dialog-footer">
           <br>
@@ -66,20 +54,23 @@
           <el-button type="primary" @click="addDO">Continue</el-button>
         </div>
       </el-dialog>
+
       <el-dialog title="edit" :visible.sync="editDialogFormVisible">
-        <el-form :model="form">
-          <el-form-item label="State">
-            <el-input v-model="editObj.state" auto-complete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="Name">
-            <el-input v-model="editObj.name" auto-complete="off"></el-input>
-          </el-form-item>
-          <el-select v-model="partyName" placeholder="party">
+        <el-form :model="editObj">
+
+          <el-form-item label="New Party Name">
+            <el-input v-model="editObj.newName" auto-complete="off"></el-input>
+          </el-form-item >
+
+          <el-form-item label="Current Party Name">
+          <el-select v-model="editObj.partyName" :disabled=true placeholder="party">
             <el-option v-for="item in partyList "
                        :key="item.partyName"
                        :label="item.partyName"
                        :value="item.partyName"></el-option>
           </el-select>
+          </el-form-item>
+
         </el-form>
         <div sloy="footer" class="dialog-footer" >
           <br>
@@ -93,21 +84,20 @@
 </template>
 
 <script>
-
+const hc = require('@/utils/httpconnect')
 export default {
 
   data() {
     return {
       addDialogFormVisible:false,
       editDialogFormVisible:false,
-      form: '',
+      form: "",
       editObj: {
-        state: '',
-        name: '',
+        partyName: "",
+        newName: ""
 
       },AddObj: {
-        state: '',
-        party: '',
+        partyName: "",
         _csrf:""
       },
       tableData: [],
@@ -115,13 +105,26 @@ export default {
     }
   },
 
+  async mounted() {
+    //get partyList
+    let response = await hc.get('/api/party')
+    for (let i=0; i < response.data.party.length;i++) {
+      let temp = {
+        partyName:response.data.party[i].partyName
+      }
+      this.partyList.push(temp)
+    }
+    console.log(response)
+  },
+
 
 
   methods: {
     handleEdit(index, row) {
-      this.tableDataIndex=row;
-      //this.editObj=index;
       this.editDialogFormVisible=true;
+      this.editObj = row;
+      const oldParty = this.editObj.partyName;
+      this.editObj.partyName = oldParty;
 
     },
     handleAdd() {
@@ -129,24 +132,80 @@ export default {
       this.addDialogFormVisible=true;
 
     },
-
-    editDo(){
-      let index=this.tableDataIndex;
-      //立即更改 若要交互 改成后台交互成功之后，再更新list
-      this.tableData[index]=this.editObj;
-      this.editDialogFormVisible=false;
+    async addDO() {
+      let CsrfResponse = await hc.get("/api/csrf");
+      this.AddObj._csrf = CsrfResponse.data.csrfToken;
+      let res = await hc.post('/api/party', this.AddObj);
+      if(res.data.status === 'success'){
+        // add success
+        this.$message({
+          type: 'success',
+          message: 'Successfully!'
+        });
+        this.addDialogFormVisible = false;
+        // refresh current page
+        location.reload();
+      }
+      else {
+        // add failed
+        this.$message({
+          type: 'error',
+          message: res.data.msg
+        });
+      }
     },
-    handleDelete(index, row){
-      console.log(index, row);
+
+    async editDo(){
+      let CsrfResponse = await hc.get("/api/csrf");
+      this.editObj._csrf = CsrfResponse.data.csrfToken;
+
+      let res = await hc.put('/api/party', this.editObj);
+      if(res.data.status === 'success'){
+        // add success
+        this.$message({
+          type: 'success',
+          message: 'Successfully!'
+        });
+        this.editDialogFormVisible = false;
+        // refresh current page
+        location.reload();
+      }else {
+        // add failed
+        this.$message({
+          type: 'error',
+          message: res.data.msg
+        });
+      }
+    },
+
+    async handleDelete(index, row){
+
+      let CsrfResponse = await hc.get("/api/csrf")
+      let _csrf = CsrfResponse.data.csrfToken;
+      let reqData = {
+        partyName: row.partyName,
+        _csrf: _csrf
+      };
+     // console.log(index, row);
+      this.$confirm('Are you sure to delete this item?', 'Notice', {
+        confirmButtonText: 'Continue',
+        cancelButtonText: 'Cancel',
+        type: 'warning'
+      })
       this.$confirm('Are you sure to delete this item?', 'Notice', {
         confirmButtonText: 'Continue',
         cancelButtonText: 'Cancel',
         type: 'warning'
       }).then(() => {
-        this.$message({
-          type: 'success',
-          message: 'Successfully!'
-        });
+        // send delete request
+        hc.delete('/api/party/',reqData).then(() => {
+          this.$message({
+            type: 'success',
+            message: 'Success!'
+          });
+          // reload page
+          location.reload();
+        })
       }).catch(() => {
         this.$message({
           type: 'info',
