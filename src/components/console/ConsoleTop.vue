6fd3ac5ffae24511d9023966ea84598c94a1b61f
humaoxuan <19950410@qq.com>
2020-10-29 12:50:03 +1030

    click logo redirect to home page

--- src/components/console/ConsoleTop.vue
+++ src/components/console/ConsoleTop.vue
@@ -1,46 +1,49 @@
 <template>
-<div>
-  <div >
-    <img src="@/assets/adelaide logo.png" alt="Adelaide logo" class="logo">
-  </div>
-    <div class = "greetings">
+  <div>
+    <div>
+      <img src="@/assets/adelaide logo.png" alt="Adelaide logo" class="logo" @click="logoHandler">
+    </div>
+    <div class="greetings">
       <span>Hi,{{ name }}</span>
       <el-button size="mini" type="text" plain @click="handleLogout">logout</el-button>
     </div>
-  <div class="line1" >
-    <hr width=100% size=3 color=#5B7B9E style="filter:alpha(opacity=100,finishopacity=0,style=2)">
+    <div class="line1">
+      <hr width=100% size=3 color=#5B7B9E style="filter:alpha(opacity=100,finishopacity=0,style=2)">
+    </div>
+    <div class="line2" style="background-color:#5B7B9E;padding:40px;margin-bottom:0px;"></div>
   </div>
-  <div class="line2" style="background-color:#5B7B9E;padding:40px;margin-bottom:0px;"></div>
-</div>
 
 </template>
 
 <script>
 const hc = require('@/utils/httpconnect');
 export default {
-name: "ConsoleTop",
+  name: "ConsoleTop",
   data() {
     return {
       name: ''
     };
-  },async mounted() {
+  }, async mounted() {
     let role = this.$cookies.get('role');
     //user
-    if(role ==='user') {
-      let user = await hc.get('api/users/'+this.$cookies.get('id'));
+    if (role === 'user') {
+      let user = await hc.get('api/users/' + this.$cookies.get('id'));
       this.name = user.data.user.name;
       return;
     }
     //admin
-    if(role ==='admin') {
+    if (role === 'admin') {
       this.name = 'Administrator';
       return;
     }
 
     //not login
     await this.$router.push({path: '/login'});
-  },methods: {
-    handleLogout(){
+  }, methods: {
+    logoHandler() {
+      this.$router.push({path: '/'});
+    },
+    handleLogout() {
       this.$cookies.remove('role');
       this.$cookies.remove('id');
       location.reload();
@@ -50,38 +53,43 @@ name: "ConsoleTop",
 </script>
 
 <style>
-.greetings{
+.greetings {
   height: 10px;
   width: 500px;
-  position:absolute;
-  top:-30px;
-  left:180px;
+  position: absolute;
+  top: -30px;
+  left: 180px;
   z-index: 999;
 }
-.greetings *{
+
+.greetings * {
   /*width: 100px;*/
   margin: 10px;
 }
-.logo{
-  width:6%;
-  height:auto;
-  position:absolute;
-  top:1%;
-  left:3%;
-  float:left;
+
+.logo {
+  width: 6%;
+  height: auto;
+  position: absolute;
+  top: 1%;
+  left: 3%;
+  float: left;
 }
-.line1{
-  position:absolute;
-  top:11%;
-  width:100%
+
+.line1 {
+  position: absolute;
+  top: 11%;
+  width: 100%
 }
-.line2{
-  position:absolute;
-  top:13%;
-  width:100%
+
+.line2 {
+  position: absolute;
+  top: 13%;
+  width: 100%
 }
-*{
-  font-family:"Arial";
+
+* {
+  font-family: "Arial";
 
 }
 </style>
